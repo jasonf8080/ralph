8264b36a0a5014d09e7b0c140b62618c05963835
jasonf8080 <87998220+jasonf8080@users.noreply.github.com>
2022-03-30 12:42:44 -0400

    Add files via upload

--- slider.js
+++ slider.js
@@ -2,7 +2,7 @@ import {cases} from "./data.js";
 
 const nextBtn = document.querySelector('.fas.fa-chevron-right');
 const prevBtn = document.querySelector('.fas.fa-chevron-left');
-const sliderImg = document.querySelector('.slider-img');
+const sliderContainer = document.querySelector('.slider-container');
 let counter = JSON.parse(localStorage.getItem('overlayIndex'));
 
 window.addEventListener('DOMContentLoaded', () =>{
@@ -13,7 +13,7 @@ nextBtn.addEventListener('click', nextImage);
 prevBtn.addEventListener('click', prevImage);
 
 function setupImage(){
-    sliderImg.src = `case${counter}.png`;
+    sliderContainer.innerHTML = `<img src="case${counter}.png" alt="" class="slider-img">`;
 }
 
 function nextImage(){
